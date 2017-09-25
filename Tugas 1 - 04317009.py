07e1dbbfbe834890bee262da47208c8c6a767443
aldoairlangga <32035845+aldoairlangga@users.noreply.github.com>
2017-09-25 13:43:50 +0700

    Tugas 1

---
+++ Tugas 1 - 04317009.py
@@ -0,0 +1,9 @@
+nama = "Aldo Airlangga"
+nim = "04317009"
+umur = 18
+print ("Namaku : " + nama)
+print ("Nimku : " + nim)
+print ("umur : ")
+print (umur)
+print ("umurku 5 tahun lagi : ")
+print (umur + 5)
